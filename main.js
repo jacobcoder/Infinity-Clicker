8cae3d5e11c22f0c78ecb33755e1a646cce3a105
jacobcoder <Jacobet12027@gmail.com>
2020-04-18 22:48:14 -0400

    fixes, fixes
    fixes everywhere

--- main.js
+++ main.js
@@ -10,16 +10,16 @@ var metaNumMult = 1
 var metanumPower = 1
 //Stage 1: The Beginning
 function normNumClick(){
-    mainNumber = mainNumber + (1*normNumMult)^normNumPower
+    mainNumber = mainNumber + math.pow((1*normNumMult), normNumPower)
     document.getElementById("mainNumber").innerHTML = "Your number is "+mainNumber+"."
 }
 //Stage 1.1: Number Click Mult Increments
 function numClickMultClick(){
     normNumMult = normNumMult + 1
-    document.getElementById("numClickMult").innerHTML = "Your number gain is multiplied by "+normNumMult+"."
+    document.getElementById("normNumMult").innerHTML = "Your number gain is multiplied by "+normNumMult+"."
 }
 //stage 1.2: Number Click Power Increments
 function numClickPowClick(){
     normNumPower = normNumPower + 1
-    document.getElementById("numClickPower").inner.html = "Your number is being exponentiated by "+normNumPower+"."
+    document.getElementById("normNumPower").inner.html = "Your number is being exponentiated by "+normNumPower+"."
 }
