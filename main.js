bb53ca5bd1c629e4e11c9ddac68a2801642dbf43
jacobcoder <Jacobet12027@gmail.com>
2020-04-18 22:37:29 -0400

    new operator
    added a new operator. pls work, expandtanum. pls work

--- main.js
+++ main.js
@@ -4,18 +4,22 @@ var infinityNumber = 0
 var metaNumber = 0
 var normNumMult = 1
 var normNumPower = 1
-var numClickMult = 1
 var infNumMult = 1
 var infNumPower = 1
 var metaNumMult = 1
 var metanumPower = 1
 //Stage 1: The Beginning
 function normNumClick(){
-    mainNumber = mainNumber + 1*numClickMult
-    document.getElementById("mainNumber").innerHTML = mainNumber
+    mainNumber = mainNumber + (1*normNumMult)^normNumPower
+    document.getElementById("mainNumber").innerHTML = "Your number is "+mainNumber+"."
 }
 //Stage 1.1: Number Click Mult Increments
 function numClickMultClick(){
-    numClickMult = numClickMult + 1
-    document.getElementById("numClickMult").innerHTML = numClickMult
+    normNumMult = normNumMult + 1
+    document.getElementById("numClickMult").innerHTML = "Your number gain is multiplied by "+normNumMult+"."
+}
+//stage 1.2: Number Click Power Increments
+function numClickPowClick(){
+    normNumPower = normNumPower + 1
+    document.getElementById("numClickPower").inner.html = "Your number is being exponentiated by "+normNumPower+"."
 }
